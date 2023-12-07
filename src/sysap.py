16426954090f1b1f8d86654f42b6b3af903975bc
Haiko Emmel <mail@haiko-emmel.de>
2023-12-07 15:24:31 +0100

    Schnittstellenfunktion zum Setzen von Datenpunkten implementiert und Hostadresse des SysAP in .env konfigurierbar.

--- src/sysap.py
+++ src/sysap.py
@@ -2,22 +2,38 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 class SysAP :
-    GET_CONFIGURATION = "http://sysap/fhapi/v1/api/rest/configuration"
-    GET_DEVICELIST ="http://sysap/fhapi/v1/api/rest/devicelist"
-    GET_DEVICE ="http://sysap/fhapi/v1/api/rest/device/"
-    GET_DATAPOINT = "http://sysap/fhapi/v1/api/rest/datapoint/"
 
-    def __init__(self, username, passwd, uuid) -> None:
+    GET_CONFIGURATION = "http://{}/fhapi/v1/api/rest/configuration"
+    GET_DEVICELIST = "http://{}/fhapi/v1/api/rest/devicelist"
+    GET_DEVICE = "http://{}/fhapi/v1/api/rest/device/"
+    GET_DATAPOINT = "http://{}/fhapi/v1/api/rest/datapoint/"
+
+    def __init__(self, hostname, username, passwd, uuid) -> None:
+        self.hostname = hostname
         self.username = username
         self.passwd = passwd
         self.uuid = uuid
 
     def __get(self, endpoint) :
-        response = requests.get(endpoint, auth=HTTPBasicAuth(self.username, self.passwd))
+
+        response = requests.get(endpoint.format(self.hostname), auth=HTTPBasicAuth(self.username, self.passwd))
 
         if response.status_code == 200:
             return response.json()
 
+        return None
+
+    def __put(self, endpoint, data) :
+
+        headers = {
+            'Content-Type': 'text/plain',
+        }
+
+        response = requests.put(endpoint.format(self.hostname), auth=HTTPBasicAuth(self.username, self.passwd), data=data, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()
+    
         return None
 
 
@@ -33,14 +49,23 @@ class SysAP :
     def get_datapoint(self, device_serial, channel, datapoint) :
         return self.__get(self.GET_DATAPOINT + self.uuid + "/" + device_serial + "." + channel + "." + datapoint)
     
+    def set_datapoint(self, device_serial, channel, datapoint, value) :
+        return self.__put(self.GET_DATAPOINT + self.uuid + "/" + device_serial + "." + channel + "." + datapoint, value)
+    
 if __name__ == "__main__" :
     from dotenv import load_dotenv
     import os
 
     load_dotenv()
 
+    hostname = os.getenv("SYSAP")
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWD")
 
-    sysap = SysAP(username, password, "00000000-0000-0000-0000-000000000000")
-    print(sysap.get_datapoint("BEEDF44C0004", "ch0000", "idp0000"))
+    sysap = SysAP(hostname, username, password, "00000000-0000-0000-0000-000000000000")
+    # print(sysap.get_datapoint("ABB2889C2851", "ch0010", "idp0000"))
+    # print(sysap.get_device("ABB2889C2851"))
+    print(sysap.set_datapoint("ABB2889C2851", "ch0010", "idp0000","0"))
+    # print(sysap.get_device("6000A1CA6FE6"))
+    # print(sysap.set_datapoint("6000A1CA6FE6", "ch0000", "odp0000","0"))
+    # print(sysap.set_datapoint("6000A1CA6FE6", "ch0000", "odp0000","0"))
